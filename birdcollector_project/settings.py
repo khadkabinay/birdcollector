900a0e5911217afdc4c1717dddfc9c310c77c155
Binay bk <khadkabinay@yahoo.com>
2020-10-04 10:18:36 -0500

    adds home and about for urls and views

--- birdcollector_project/settings.py
+++ birdcollector_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+# from secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-
-from secrets import *
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'e4fp6)ecyem89qqh7d*@ly5&!c0y90wn^wqq9&ez&(77r2ay%8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +77,9 @@ WSGI_APPLICATION = 'birdcollector_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'birdcollector',
+        'USER': 'binay'
     }
 }
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,37 @@
+# Add the following import
 from django.shortcuts import render
+from django.http import HttpResponse 
+
 
 # Create your views here.
+# Define the home view
+def home(request):
+    return HttpResponse('<h1>Hello Birds</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+
+
+class Bird:
+    def __init__(self, name,description,color, age):
+        self.name = name
+        self.description = description
+        self.color = color
+        self.age = age
+
+
+birds = [
+Bird('Parrots', 'also known as psittacines and found mostly in tropical and subtropical regions', 'red and blue', 2),
+Bird('Owls', 'tube-shaped eyes are completely immobile, providing binocular vision which fully focuses on their prey and boosts depth perception', 'dark-dotted and grey', 1),
+Bird('Songbirds', 'another name that is sometimes seen as the scientific or cernacular name is Oscines Latin Oscen A song bird', 'yellow and dark', 3)
+]
+
+
+
+
+
+
+def birds_index(request):
+    context = {'birds':birds}
+    return render(request, 'birds/index.html' , context)
