8b2a44d49a14d3769b0b2f55aab2764611b8088c
Binay bk <khadkabinay@yahoo.com>
2020-10-04 18:39:06 -0500

    adds detail.html on templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('birds/', views.birds_index, name='birds_index'),
+    path('birds/', views.birds_index, name='index'),
+    path('birds/<int:bird_id>/', views.birds_detail, name='detail')
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,6 @@ from .models import Bird
 
 
 # Create your views here.
-# Define the home view
 def home(request):
     return HttpResponse('<h1>Hello Birds</h1>')
 
@@ -14,26 +13,13 @@ def about(request):
 
 
 
-# class Bird:
-#     def __init__(self, name,description,color, age):
-#         self.name = name
-#         self.description = description
-#         self.color = color
-#         self.age = age
-
-
-# birds = [
-# Bird('Parrots', 'also known as psittacines and found mostly in tropical and subtropical regions', 'red and blue', 2),
-# Bird('Owls', 'tube-shaped eyes are completely immobile, providing binocular vision which fully focuses on their prey and boosts depth perception', 'dark-dotted and grey', 1),
-# Bird('Songbirds', 'another name that is sometimes seen as the scientific or cernacular name is Oscines Latin Oscen A song bird', 'yellow and dark', 3)
-# ]
-
-
-
-
-
 
 def birds_index(request):
     birds = Bird.objects.all()
     context = {'birds':birds}
     return render(request, 'birds/index.html' , context)
+
+
+def birds_detail(request,bird_id):
+    bird = Bird.objects.get(id=bird_id)
+    return render(request, 'birds/detail.html',{'bird':bird})
