c543bfc046ce08e5cf28ad49869307d47b6abb06
Binay bk <khadkabinay@yahoo.com>
2020-10-06 23:28:15 -0500

    adds Feeding model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # import your models here
-from .models import Bird
+from .models import Bird, Feeding
 
 # Register your models here.
 admin.site.register(Bird)
+admin.site.register(Feeding)
 
